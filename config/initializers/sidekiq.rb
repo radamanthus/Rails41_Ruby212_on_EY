32ab635a17215d549b0537e908541da5b7badf61
Radamanthus Batnag <rad@batnag.org>
2014-11-21 16:37:30 +0800

    hard-code redis IP for now

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis_instance:6379' }
+  config.redis = { url: 'redis://ip-10-231-191-5:6379' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis_instance:6379' }
+  config.redis = { url: 'redis://ip-10-231-191-5:6379' }
 end
