2d277ee04dbdb47771462201b8a311bce185391d
Radamanthus Batnag <rad@batnag.org>
2014-11-24 09:04:24 +0800

    Load redis config to initialize sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+redis_config = YAML.load_file('config/redis.yml')[Rails.env]
+host = redis_config["host"]
+port = redis_config["port"]
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://ip-10-231-191-5:6379' }
+  config.redis = { url: "redis://#{host}:#{port}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://ip-10-231-191-5:6379' }
+  config.redis = { url: "redis://#{host}:#{port}" }
 end
