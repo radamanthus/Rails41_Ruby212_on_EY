8b157c141e214e8ae7a3dc7b20f48f4cda36714e
Radamanthus Batnag <rad@batnag.org>
2014-11-24 10:33:28 +0800

    Update release_version in Redis

---
+++ config/deploy/after_restart.rb
@@ -0,0 +1,14 @@
+# Update release_version in Redis
+
+require 'yaml'
+require 'redis'
+
+rails_env = config.framework_env
+redis_config = YAML.load_file('config/redis.yml')[rails_env]
+host = redis_config['host']
+port = redis_config['port']
+
+current_version = File.expand_path(__FILE__).scan(/\d{10,}/).map(&:to_i)[0] || -1
+conn = Redis.new(host: host, port: port)
+conn.set "release_version", current_version
+
